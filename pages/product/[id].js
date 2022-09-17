40f8f8b7be7448b959c4b382069c2833dd2191e9
Alex Mijail Boulchouk <amboulchouk@outlook.com>
2022-09-17 16:08:23 -0300

    adding Next.js in the project

---
+++ pages/product/[id].js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { useRouter } from 'next/router';
+
+const ProductItem = () => {
+  return (
+    <div>Esta es la pagina del producto</div>
+  )
+}
+
+export default ProductItem
---
+++ pages/usuarios/nana.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nana = () => {
+  return (
+    <div>
+        <h2>Usuario Nana</h2>
+
+    </div>
+  )
+}
+
+export default Nana
